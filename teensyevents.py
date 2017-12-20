d8450bc5e1f6fdb5c414d620e69f214b7f403018
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2017-12-20 11:45:58 +0100

    Add copyright notices to the header of the files

--- teensyevents.py
+++ teensyevents.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# This file is part of pyteensy.
+# 
+# pyteensy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+# 
+# pyteensy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with pyteensy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 ''' This is the teensyevents program. It is intended to make a connection
 with a teensy device. It registers some input lines in order to make
 output the events that occur on that line of the teensy.
